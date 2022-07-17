d5fbaadba2931ddf329b77e7b2bc1fc239a4f908
Samuel Bostick <me@samuelbostick.com>
2022-07-17 16:50:24 -0400

    feat: Create Task

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
 const App: React.FC = () => {
+  const [tasks, setTasks] = React.useState<string[]>([])
+
+  function handleAdd() {
+    var task = prompt("Please enter task details")
+    if (task) {
+      setTasks([...tasks, task])
+    }
+  }
+
   return (
     <div>
+      <button onClick={handleAdd}>Add</button>
+      <ul>
+        {
+          tasks.map(task => <li key={task}>{task}</li>)
+        }
+      </ul>
     </div>
   );
 }
